8f6c64529c4bef8d3df836f1c2f08115f8bcb034
Dmitry <manxxeta@gmail.com>
2018-10-19 13:37:25 -0700

    editing

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 'use strict';
 
 require('dotenv').config();
+
 const express = require('express');
+const request = require('request');
 const app = express();
 const port = 3000;
 
@@ -10,21 +12,14 @@ app.get('/', (req, res) => res.send('Hello, Alex!'));
 /*-------------------------------------------------------------*/
 //        GET
 /*-------------------------------------------------------------*/
-app.get('/tweets', (req, res) => {
 
-  res.send('something');
+app.get('/tweets', (request, response) => {
 
-
-
+  let arrayOfTweets = [];
   const twitter_api = 'https://api.twitter.com/1.1/search/tweets.json?geocode=37.781157,-122.398720,1mi';
+  const bearer_token = process.env.TWITTER_BEARER_TOKEN;
 
-
-  var request = require('request');
-  var bearer_token = process.env.TWITTER_BEARER_TOKEN;
-
- 
-
-  var options = {
+  const options = {
     method: 'GET',
     url: twitter_api,
     qs: {
@@ -36,13 +31,24 @@ app.get('/tweets', (req, res) => {
     }
   };
 
+
   request(options, function(error, response, body) {
+
+    // Here we need to construct a 'tweet' object and push it to an array
     console.dir(body);
   });
 
 
 
 });
+
+
+
+
+/*-------------------------------------------------------------*/
+//        ENTRY POINT
+/*-------------------------------------------------------------*/
+
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
 
