154b2de5fdd6084cbeef875a02769034fb11f9a8
Dmitry <manxxeta@gmail.com>
2018-10-19 12:50:41 -0700

    editing

--- index.js
+++ index.js
@@ -1,32 +1,44 @@
 'use strict';
 
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello, Alex!'));
-
 app.get('/tweets', (req, res) => {
 
   res.send('something');
 
-  /*---------------------------------------------------------------*/
-  /*                        TWITTER API                            */
-  /*---------------------------------------------------------------*/
-   const twitter_api_proxy = `https://api.twitter.com/1.1/search/tweets.json?q=sobaka`;
-   geocode = long, lat, radius
 
-   const twitter_api_proxy = `https://api.twitter.com/1.1/search/tweets.json?geocode=37.781157,-122.398720,1mi`;
-   const bearer_token_proxy = config.REACT_APP_TWITTER_BEARER_TOKEN_TEXT;
-   fetch(twitter_api_proxy, {
-     headers: {
-       "Authorization": "Bearer " + bearer_token_proxy
-     }
-   })
-   .then(response => response.json())
-   .then(response => {
-     console.log(response);
-   })
+
+  const twitter_api = 'https://api.twitter.com/1.1/search/tweets.json?geocode=37.781157,-122.398720,1mi';
+
+
+  var request = require('request');
+  var bearer_token = process.env.TWITTER_BEARER_TOKEN;
+
+  console.log(bearer_token);
+
+//   var options = {
+//     method: 'GET',
+//     url: twitter_api,
+//     qs: {
+//       'screen_name': 'twitterapi'
+//     },
+//     json: true,
+//     headers: {
+//       'Authorization': 'Bearer ' + bearer_token
+//     }
+//   };
+
+//   request(options, function(error, response, body) {
+//     console.dir(body);
+//   });
+
+
+
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
+
