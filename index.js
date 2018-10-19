166be6d347ca9e3024b9beeaa4510e48c6327cf7
Dmitry <manxxeta@gmail.com>
2018-10-19 11:59:37 -0700

    editing

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello, Alex!'));
+
+app.get('/tweets', (req, res) => {
+
+  res.send('something');
+
+  /*---------------------------------------------------------------*/	
+    /*            This is works with a proxy but super slow          */	
+    /*                        TWITTER API                            */	
+    /*---------------------------------------------------------------*/	
+     // const twitter_api_proxy = `https://cors-anywhere.herokuapp.com/https://api.twitter.com/1.1/search/tweets.json?q=sobaka`;	
+     // geocode = long, lat, radius 	
+    // const twitter_api_proxy = `https://cors-anywhere.herokuapp.com/https://api.twitter.com/1.1/search/tweets.json?geocode=37.781157,-122.398720,1mi`;	
+    // const bearer_token_proxy = config.REACT_APP_TWITTER_BEARER_TOKEN_TEXT;	
+    // fetch(twitter_api_proxy, {	
+    //   headers: {	
+    //     "Authorization": "Bearer " + bearer_token_proxy	
+    //   }	
+    // })	
+    // .then(response => response.json())	
+    // .then(response => {	
+      	
+    //   console.log(response);	
+    // })
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
