4f96f059686a44e2f6d9cc58f0d026b5a59b27a9
Dmitry <manxxeta@gmail.com>
2018-10-24 09:51:16 -0700

    final cors fix

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 /*-------------------------------------------------------------*/
+//        CORS REQUEST FIX
+/*-------------------------------------------------------------*/
+
+app.use((request, response, next) => {
+
+  response.header('Access-Control-Allow-Origin', '*');
+  response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+/*-------------------------------------------------------------*/
 //        GET
 /*-------------------------------------------------------------*/
 
